9995839b9bdb09e8caba268519a792366ade3862
nikolailposto <n.milojkov.3572@LAPLACE-24>
2025-01-13 08:32:19 +0100

    commit

---
+++ Informatica/Forza4_Console/Forza4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forza4_Console
+{
+    internal class Forza4
+    {
+        public static void inizializzacampo(char[,] carlo)
+        {
+            for (int i = 0; i < carlo.GetLength(0); i++)
+            {
+                for (int j = 0; j < carlo.GetLength(1); j++)
+                {
+                    carlo[i, j] = '-';
+                }
+            }
+        }
+
+        public static void stampamatrice(char[,] carlo)
+        {
+            Console.Clear();
+
+            for (int i = 0; i < carlo.GetLength(0); i++)
+            {
+                for (int j = 0; j < carlo.GetLength(1); j++)
+                {
+                    Console.Write($"{carlo[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Informatica/Forza4_Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Forza4_Console.Forza4;
+
+namespace Forza4_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] Carlo = new char[6, 7];
+            char giocatorecorrente = 'X';
+
+            do
+            {
+                Carlo = new char[6, 7];
+                giocatorecorrente = 'X';
+                inizializzacampo(Carlo);
+                stampamatrice(Carlo);
+                //Inizio partita
+                do
+                {
+
+                } while (true);
+
+
+            } while (true);
+        }
+    }
+}
