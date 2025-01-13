6a9dcae1a981f09737893b80c5f805f0f02dc2d8
nikolailposto <n.milojkov.3572@LAPLACE-24>
2025-01-13 09:08:43 +0100

    Checkpoint before win condtion

---
+++ Informatica/Forza4_Paschetta/Forza4.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forza4_Console
+{
+    internal class Forza4
+    {
+        public static void inizializzacampo(char[,] carlo)
+        {
+            for (int i = 0; i < carlo.GetLength(0); i++)
+            {
+                for (int j = 0; j < carlo.GetLength(1); j++)
+                {
+                    carlo[i, j] = '-';
+                }
+            }
+        }
+        public static int posizionaGettone(char[,] carlo, char giocatoreCorrente, int colums)
+        {
+            int i = carlo.GetLength(1) - 1;
+
+            while (carlo[i, colums] == '-')//Ho gia verificato che il gettone poss essere piazzato
+            {
+                i--;
+            }
+            carlo[i, colums] = giocatoreCorrente;
+            return i;
+        }
+
+        public static void stampamatrice(char[,] campo)
+        {
+            Console.Clear();
+
+            // Stampo la parte superiore del bordo 
+            Console.WriteLine("  -".PadRight(29, '-'));
+
+            for (int i = 0; i < campo.GetLength(0); i++)
+            {
+                Console.Write(" | ");
+                for (int j = 0; j < campo.GetLength(1); j++)
+                {
+                    Console.Write(campo[i, j] + " | ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("  -".PadRight(29, '-')); // linea separazione tra righe
+            }
+        }
+
+        public static bool WinCheck(char[,] carlo, int rows, int colums, char giocatoreCorrente)
+        {
+            bool vittoria = false;
+            int cnt;
+
+
+
+
+        }
+    }
+}
---
+++ Informatica/Forza4_Paschetta/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Forza4_Console.Forza4;
+
+namespace Forza4_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] Carlo;
+            char giocatoreCorrente;
+            int colums,rows;
+            bool vittoria;
+
+            do
+            {
+                vittoria = false;
+                Carlo = new char[6, 7];
+                giocatoreCorrente = 'X';
+                inizializzacampo(Carlo);
+                stampamatrice(Carlo);
+                //Inizio partita
+                do
+                {
+                    do
+                    {
+
+                        Console.Write("Giocatore " + giocatoreCorrente + ", " +"scegli colonna (0-6) > ");
+
+                    } while (!int.TryParse(Console.ReadLine(), out colums) || colums < 0 || colums > 6);
+
+                    if (Carlo[0,colums] == '-')
+                    {
+                        rows = posizionaGettone(Carlo, giocatoreCorrente, colums);
+                        stampamatrice(Carlo);
+                        giocatoreCorrente = giocatoreCorrente == 'X' ? 'O' : 'X';
+
+                        vittoria = WinCheck(Carlo, rows, colums, giocatoreCorrente);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Colonna piena. Riprova.");
+                    }
+                } while (!vittoria);
+
+
+            } while (true);
+        }
+    }
+}
