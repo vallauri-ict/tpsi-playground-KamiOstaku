fa98ade2edcbb2abfe897499f7ee2e6b62ca41c7
KamiOstaku <n.milojkov.3572@vallauri.edu>
2025-01-16 00:18:32 +0100

    Informatica exercise (isnt complete cause the lazyness of implementation of an matrix

---
+++ Informatica/Controllo Sotto Diagonale Principale/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllo_Sotto_Diagonale_Principale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
