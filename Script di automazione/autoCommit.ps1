b531b42c083a2204c339143e1490be111dec4e10
KamiOstaku <n.milojkov.3572@vallauri.edu>
2025-04-29 20:58:27 +0200

    Commit on Logout

---
+++ Script di automazione/autoCommit.ps1
@@ -0,0 +1,5 @@
+cd "Path della tua cartella con git"
+
+git add .
+git commit -m "Messaggio di commit"
+git push origin main
--- Script di automazione/commitOnLogout.bat
+++
@@ -1,7 +0,0 @@
-cd "Aggiungere path della cartella git"
-
-git add .
-
-git commit -m "Commit on Logout"
-
-git push
