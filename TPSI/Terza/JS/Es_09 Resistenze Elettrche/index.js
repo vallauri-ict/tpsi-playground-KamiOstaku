f06e55788f0c1fe6f8b3a1469d5663d83156df9e
KamiOstaku <n.milojkov.3572@vallauri.edu>
2025-05-01 15:37:22 +0200

    another fix on TPSI/Terza/JS directory

--- TPSI/Terza/JS/Es_09 Resistenze Elettrche/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const colori = ["Argento", "Oro", "Nero", "Marrone", "Rosso", "Arancio", "Giallo",
-                "Verde", "Blu", "Viola", "Grigio", "Bianco" ];
-				
-const tolleranze=["10", "5", "", "1", "2", "", "", "0.5", "0.25", "0.1", "", ""];
-
-
